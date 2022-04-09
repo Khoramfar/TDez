0e51e4cc21523267640c7e958d0ea019f3025fd9
Ali-kh1998 <alikhorramfar@gmail.com>
2022-04-10 03:18:17 +0430

    migrate shows table

---
+++ database/migrations/2022_04_10_030614_create_shows_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shows', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('theater_id');
+            $table->unsignedBigInteger('salon_id');
+            $table->unsignedBigInteger('admin_id');
+
+            $table->datetime('show_date');
+            $table->boolean('public')->default(false);
+            $table->timestamps();
+
+            $table->foreign('theater_id')->references('id')->on('theaters');
+            $table->foreign('salon_id')->references('id')->on('salons');
+            $table->foreign('admin_id')->references('id')->on('users');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shows');
+    }
+};

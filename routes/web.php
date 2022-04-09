b779fbcf5b95eb71bde4d29e4943358dbe448098
Ali-kh1998 <alikhorramfar@gmail.com>
2022-04-10 02:31:01 +0430

    before migrations

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+require __DIR__.'/auth.php';
 Route::get('/', function () {
     return view('welcome');
 });
@@ -21,4 +21,4 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-require __DIR__.'/auth.php';
+
